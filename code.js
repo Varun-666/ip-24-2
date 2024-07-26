352e935210d97c0b6b83ffc4c35e36051b0b8356
Varun-666 <varunfatehpuria2@gmail.com>
2024-07-26 12:08:03 +0530

    Updated

--- code.js
+++ code.js
@@ -3,6 +3,6 @@ import { upperCase as uc } from 'upper-case';
 
 http.createServer( (req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(uc("Hello World!"));
+    res.write(uc("Hello World! This is a new sever"));
     res.end();
 }).listen(8080);
